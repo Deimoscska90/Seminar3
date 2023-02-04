c009972d478f043993eeca0e1bd7dcdc73a200f6
Filipp <Filipp_nikolaev@rambler.ru>
2023-02-04 17:28:15 +0600

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int GetNumber(string message)
+
+{
+int result = 0;
+
+while (true)
+{
+Console.WriteLine(message);
+
+if (int.TryParse(Console.ReadLine(), out result))
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число или 0. Повторите ввод");
+}
+}
+}
+
+int number = GetNumber("Введите пятизначное число:");
+if (number%10 == number/10000 && number%100 == number/1000);
+Console.Write("ДА");
+else
+Console.Write("НЕТ");
